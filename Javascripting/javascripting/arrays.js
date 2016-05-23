e673b8614725ad662c6794759e547ab55c20e328
scott.roylance <docroylance@hotmail.com>
2016-05-23 15:00:27 -0500

    Simple Array and For-Loop for element returns

---
+++ Javascripting/javascripting/arrays.js
@@ -0,0 +1,8 @@
+﻿var pizzaToppings = ['tomato sauce','cheese','pepperoni'];
+//console.log(pizzaToppings[0]);
+console.log(pizzaToppings);
+
+//TO RETURN THE ELEMENTS OF AN ARRAY WITHOUT ARRAY SYNTAX ([,'',])
+//for (var i = 0; i < pizzaToppings.length; i++) {
+//    console.log(pizzaToppings[i]);
+//};
