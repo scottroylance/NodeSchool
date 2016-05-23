d80624fe3074ceb87d799d85f02514f4a0d5a35b
scott.roylance <docroylance@hotmail.com>
2016-05-23 13:56:42 -0500

    For loop completed

---
+++ Javascripting/javascripting/for-loop.js
@@ -0,0 +1,10 @@
+﻿var total = 0;
+var limit = 10;
+
+for (var i = 0; i < limit; i++) {
+    total += i;
+    //console.log('Total is now: ' + total);
+};
+
+//console.log('The grand total is ' + total);
+console.log(total);
