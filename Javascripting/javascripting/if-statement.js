ef105e56b27f92b751a1dda325c8bc6da7d6e347
scott.roylance <docroylance@hotmail.com>
2016-05-23 13:19:23 -0500

    if statments complete.

---
+++ Javascripting/javascripting/if-statement.js
@@ -0,0 +1,10 @@
+﻿var fruit = 'orange';
+//console.log(fruit.length);
+
+if (fruit.length > 5) { console.log('The fruit name has more than five characters.')
+    } 
+    else {console.log('The fruit name has five characters or less.')
+};
+
+//when comparing a var with a string value, you need to add a method to the var in order for any comparison to be made.  The computer does
+//not know how to evaluate string otherwise.
